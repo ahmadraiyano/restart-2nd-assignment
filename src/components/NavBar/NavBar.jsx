105b9a6c8b9cd269fbdfd80cd41af5b924b04a1b
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-03-06 20:45:19 +0600

    bg and text color changed in burger menu for mobile screen size

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -37,7 +37,7 @@ const NavBar = () => {
             </div>
             <ul
               tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow text-white"
+              className="menu menu-sm dropdown-content bg-white rounded-box z-1 mt-3 w-52 p-2 shadow text-black"
             >
               <NavLi></NavLi>
             </ul>
